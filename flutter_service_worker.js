fc2233e741fccd73b9d48beb7964f1bc963c9b6c
AmbujaAK <itsambuja@gmail.com>
2020-12-15 01:37:14 +0530

    filter added to projects

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "fd95d7158de812d7722d38c3aac88721",
-"/": "fd95d7158de812d7722d38c3aac88721",
-"main.dart.js": "cddc8d132592af6fb62422466072b615",
+"index.html": "608f5b74d6d032b9500b588f766b3643",
+"/": "608f5b74d6d032b9500b588f766b3643",
+"main.dart.js": "ac2e4961fc33167f07c8a2ef3e78724b",
 "manifest.json": "e5aaa4a5fa6f38e68aed54f70d84dba9",
 "version.json": "9f4c1649ebd6c18f9a6e9fe0333cd841"
 };
